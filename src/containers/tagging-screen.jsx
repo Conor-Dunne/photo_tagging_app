1f2e81132c5772007e305aa0de5b86a541f7fd28
Conor <conordunne@outlook.ie>
2023-02-14 11:52:25 +0900

    Add pop-up box for selections

--- src/containers/tagging-screen.jsx
+++ src/containers/tagging-screen.jsx
@@ -1,14 +1,27 @@
 import TagMenuBox from "../components/TagMenuBox";
+import { useState } from 'react';
 
 function TaggingScreen() {
+        const [x, setX] = useState("20px")
+        const [y, setY] = useState("100px")
+        const [test, setTest] = useState("test");
 
+    function handleClick(e) {
+        setX(`${e.nativeEvent.layerX}`)
+        setY(`${e.nativeEvent.layerY}`)
+        setTest(`${x}, ${y}`)
+    }
 
-
-    return ( <div style={{
+    return ( <div
+        onClick={handleClick}
+        style={{
         position: "relative",
+        border: "1px solid blue",
+        height: "200px",
+        width: "200px"
     }}>
-        tag me
-        <TagMenuBox posX={100} posY={100}/>
+        {test}
+        <TagMenuBox posX={x} posY={y}/>
     </div> );
 }
 
