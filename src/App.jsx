93e861c60a687e09aa9e47cf4d0cb2daf2800e3d
Conor Dunne <conordunne@outlook.ie>
2023-03-17 22:26:21 +0900

    Add start screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import TaggingScreen from './containers/tagging-screen'
 import Header from './components/Header'
---
+++ src/containers/StartScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StartScreen = () => {
+  return (
+    <div className=' bg-slate-500 h-full w-full absolute top-0 left-0 z-10'>
+      <h1>Can you find them?</h1>
+    </div>
+  )
+}
+
+export default StartScreen
